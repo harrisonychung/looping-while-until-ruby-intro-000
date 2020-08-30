d0d42926e8554e7b521142d4f2a68970dbdb43b8
Harrison Chung <harrison.y.chung@gmail.com>
2020-08-30 16:53:33 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,5 +2,5 @@ def using_while
   levitation_force = 6
 while levitation_force < 10
   levitation_force+=1
-  puts "The current levitation force level is #{levitation_force}."
+  puts "The desired phrase while your levitation force is less than #{levitation_force}."
 end
