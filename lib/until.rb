e29153ceab00f3324d77fbfaec9084032eafad01
Harrison Chung <harrison.y.chung@gmail.com>
2020-08-30 16:51:21 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
 until levitation_force == 10
-puts "The current levitation force level is #{levitation_force}."
+puts "The current levitation force level is less than 10."
   end
 
 
